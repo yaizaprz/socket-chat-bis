00e6823d40e6ba6a220407934f908fcf969c956c
Yaiza Perez <yaiza.perez@igmweb.com>
2022-11-18 13:52:10 +0100

    Chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,23 +1,27 @@
 var socket = io();
 
-var params= new URLSearchParams(window.location.search);
+var params = new URLSearchParams(window.location.search);
 
-if(!params.has('nombre') || !params.has('sala')){
-  window.location='index.html';
-  throw new Error ('EL nombre y sala son necesario');
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
-  nombre: params.get('nombre'),
-  sala: params.get('sala'),
-}
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function( resp ){
-      console.log('Usuarios conectados', resp);
+    socket.emit('entrarChat', usuario, function(resp) {
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -29,26 +33,29 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-/*socket.emit('crearMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});*/
-
+ //socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-    console.log('Servidor:', mensaje);
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false); //falso porque no soy yo quien esta mandando el mensaje aqui
+    scrollBottom();
 });
 
-//Escuchar cambios de usuarios 
-//cuando un usuario entra o sale del chat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersonas', function(personas) {
-  console.log('Servidor:', personas);
+    renderizarUsuarios(personas);
 });
 
-//Mensajes privados
-socket.on('mensajePrivado', function(mensaje) { //accion del cliente de escuchar un mensaje privado
-  console.log('mensaje privado:', mensaje);
-})
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
+
+    console.log('Mensaje Privado:', mensaje);
+
+});
